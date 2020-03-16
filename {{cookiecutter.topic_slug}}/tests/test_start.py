662c2e4e62162afb983541d8cc199994cab5b577
Jeremy Kao <imsardine@gmail.com>
2020-03-16 12:23:51 +0800

    Update default tests for multi-container apps

--- {{cookiecutter.topic_slug}}/tests/test_start.py
+++ {{cookiecutter.topic_slug}}/tests/test_start.py
@@ -1,2 +1,5 @@
-def test_hello_world():
-    pass
+import json
+
+def test_hello_world(workspace):
+    r = workspace.run("curl -G --data-urlencode 'greeting=Hello, World!' http://httpbin/get")
+    assert json.loads(r.out)['args']['greeting'] == 'Hello, World!'
